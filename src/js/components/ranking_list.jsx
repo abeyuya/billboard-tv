a5aa3a60d36bb1f49bbd830d6e2240f7941ad3e8
abeyuya <yuya.abe.0525@gmail.com>
2016-01-05 00:02:03 +0900

    サムネイル画像取得を追加

--- src/js/components/ranking_list.jsx
+++ src/js/components/ranking_list.jsx
@@ -8,11 +8,13 @@ export default class RankingList extends React.Component {
   render() {
     // console.log('ranking_list:' + this.props.ranking_list);
     var list = this.props.ranking_list.map(function(record){
+      var url = 'http://i.ytimg.com/vi/' + record.video_id + '/default.jpg';
       return (
         <li key={record.rank} onClick={
             function(){ this.props.onTapSong(record.rank) }.bind(this)
           }>
           {record.rank}: {record.artist} : {record.title}
+          <img src={url} />
         </li>
       );
     }, this);
