ff71ca495fc47c372cfbf1f550c90340e1342b5b
abeyuya <yuya.abe.0525@gmail.com>
2016-01-04 23:44:27 +0900

    キャッシュ設定をなくした

--- gulpfile.js
+++ gulpfile.js
@@ -102,7 +102,7 @@ gulp.task('publish', function() {
  
     // publisher will add Content-Length, Content-Type and headers specified above
     // If not specified it will set x-amz-acl to public-read by default
-    .pipe(publisher.publish(headers))
+    // .pipe(publisher.publish(headers))
     // create a cache file to speed up consecutive uploads
     .pipe(publisher.cache())
      // print upload updates to console
