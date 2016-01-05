2bb8b15a94d56b74c5303c20b0e9321541275053
abeyuya <yuya.abe.0525@gmail.com>
2016-01-06 00:26:23 +0900

    とりあえずreact-routerが動くところまで

--- src/js/application.jsx
+++ src/js/application.jsx
@@ -1,8 +1,16 @@
-import React    from 'react';
-import ReactDom from 'react-dom';
-import RootView from './components/root_view.jsx';
+import React    from 'react'
+import ReactDom from 'react-dom'
+import Layout   from './components/layout.jsx'
+import RootView from './components/root_view.jsx'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-ReactDom.render(
-  <RootView />,
+ReactDom.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={RootView} />
+      <Route path="about" component={RootView} />
+      <Route path="inbox" component={RootView} />
+    </Route>
+  </Router>),
   document.getElementById('container')
 );
