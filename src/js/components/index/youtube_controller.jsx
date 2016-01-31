6a1462d8ee7866d7ffe1d83e0958ceaedcfb0634
abeyuya <yuya.abe.0525@gmail.com>
2016-01-31 23:54:28 +0900

    styling controller view

--- src/js/components/index/youtube_controller.jsx
+++ src/js/components/index/youtube_controller.jsx
@@ -6,11 +6,23 @@ export default class YoutubeController extends React.Component {
   };
   
   render(){
+    var repeat_class = 'youtube_controller--circle';
+    if (this.props.repeat) repeat_class += '--active';
+    
+    var random_class = 'youtube_controller--circle';
+    if (this.props.random) random_class += '--active';
+    
     return(
-      <div>
-        <a onClick={this.props.onTapRepeat}>REPEAT</a>
-        <a onClick={this.props.playNextVideo}>NEXT</a>
-        <a onClick={this.props.onTapRandom}>RANDOM</a>
+      <div className="youtube_controller">
+        <div className={repeat_class} onClick={this.props.onTapRepeat}>
+          <div>REPEAT</div>
+        </div>
+        <div className="youtube_controller--circle" onClick={this.props.playNextVideo}>
+          <div>NEXT</div>
+        </div>
+        <div className={random_class} onClick={this.props.onTapRandom}>
+          <div>RANDOM</div>
+        </div>
       </div>
     );
   };
